95776bb03b30c0e63795f2ec4cf33a975dd81d83
Alexa Cristina <ac@mysite.org>
2021-06-18 10:40:56 +0300

    feat: Run JS in the head, after DOMContentLoaded

--- js/app.js
+++ js/app.js
@@ -13,177 +13,193 @@
  *
  */
 
-/**
- * Define Global Variables
- *
- */
+// Main function
+const main = () => {
+  // Test performance start
+  const startTime = performance.now();
 
-const sectionsList = document.getElementsByTagName("section");
-const mainContent = document.querySelector("main");
-const navigationMenuList = document.getElementById("navbar__list");
+  /**
+   * Define Global Variables
+   *
+   */
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
+  const sectionsList = document.getElementsByTagName("section");
+  const mainContent = document.querySelector("main");
+  const navigationMenuList = document.getElementById("navbar__list");
 
-/**
- * @description Create new menu item
- * @param {Object} section - newly created section
- */
-const createMenuItem = (section) => {
-  const menuItem = document.createElement("li");
-  const menuLink = document.createElement("a");
+  /**
+   * End Global Variables
+   * Start Helper Functions
+   *
+   */
 
-  menuLink.setAttribute("href", `#${section.id}`);
-  menuLink.textContent = section.querySelector("h2").innerHTML;
-  menuLink.className = "menu__link";
+  /**
+   * @description Create new menu item
+   * @param {Object} section - newly created section
+   */
+  const createMenuItem = (section) => {
+    const menuItem = document.createElement("li");
+    const menuLink = document.createElement("a");
 
-  menuItem.appendChild(menuLink);
+    menuLink.setAttribute("href", `#${section.id}`);
+    menuLink.textContent = section.querySelector("h2").innerHTML;
+    menuLink.className = "menu__link";
 
-  return menuItem;
-};
+    menuItem.appendChild(menuLink);
 
-/**
- * @description Add menu item
- * @param {Object} section - newly created section
- */
-const addMenuItem = (section) => {
-  const menuItem = createMenuItem(section);
-  navigationMenuList.appendChild(menuItem);
-};
+    return menuItem;
+  };
 
-/**
- * @description Remove menu item
- * @param {Object} section - deleted section
- */
-const removeMenuItem = (section) => {
-  const menuLink = document.querySelector(`[href^="#${section.id}"]`);
-  const menuItem = menuLink.parentElement;
-  navigationMenuList.removeChild(menuItem);
-};
+  /**
+   * @description Add menu item
+   * @param {Object} section - newly created section
+   */
+  const addMenuItem = (section) => {
+    const menuItem = createMenuItem(section);
+    navigationMenuList.appendChild(menuItem);
+  };
 
-/**
- * @description Check the current section is in the viewport
- * @param {Object} section - current section
- */
-const isSectionInViewport = (section) => {
-  const position = section.getBoundingClientRect();
+  /**
+   * @description Remove menu item
+   * @param {Object} section - deleted section
+   */
+  const removeMenuItem = (section) => {
+    const menuLink = document.querySelector(`[href^="#${section.id}"]`);
+    const menuItem = menuLink.parentElement;
+    navigationMenuList.removeChild(menuItem);
+  };
 
-  // partially visible
-  // position.top < window.innerHeight && position.bottom >= 0
-  // TODO: treat case on mobile when only one section in the viewport
-  // fully visible
-  // position.top >= 0 && position.bottom <= window.innerHeight
-  return position.top >= 0 && position.bottom <= window.innerHeight;
-};
+  /**
+   * @description Check the current section is in the viewport
+   * @param {Object} section - current section
+   */
+  const isSectionInViewport = (section) => {
+    const position = section.getBoundingClientRect();
 
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
+    // partially visible
+    // position.top < window.innerHeight && position.bottom >= 0
+    // TODO: treat case on mobile when only one section in the viewport
+    // fully visible
+    // position.top >= 0 && position.bottom <= window.innerHeight
+    return position.top >= 0 && position.bottom <= window.innerHeight;
+  };
 
-// Build the navigation menu on page load
+  /**
+   * End Helper Functions
+   * Begin Main Functions
+   *
+   */
 
-/**
- * @description Populate menu list from sections list on page load
- * @param {Object} sectionsList - all sections on page
- */
-const createMenuItems = (sectionsList) => {
-  const tempList = document.createDocumentFragment();
+  // Build the navigation menu on page load
+
+  /**
+   * @description Populate menu list from sections list on page load
+   * @param {Object} sectionsList - all sections on page
+   */
+  const createMenuItems = (sectionsList) => {
+    const tempList = document.createDocumentFragment();
+
+    for (const section of sectionsList) {
+      const menuItem = createMenuItem(section);
+
+      tempList.append(menuItem);
+    }
+
+    return tempList;
+  };
+
+  /**
+   * @description Update menu on section add/remove
+   * @param {Object} event - DOMNodeRemoved or DOMNodeInserted
+   */
+  const updateMenuList = (event) => {
+    const section = event.target;
+
+    // check that the target node is a section
+    if (section.id.match(/^section/i)) {
+      if (event.type === "DOMNodeRemoved") {
+        // remove existing menu item corresponding to deleted section
+        removeMenuItem(section);
+      } else if (event.type === "DOMNodeInserted") {
+        // add menu item for added section
+        addMenuItem(section);
+      }
+    }
+  };
+
+  // Add class 'active' to section when near top of viewport
+
+  /**
+   * @description Add class 'active' to the section in viewport
+   * @param {Object} event - scroll
+   */
+  const toggleSectionActive = (section) => {
+    if (isSectionInViewport(section)) {
+      section.classList.add("your-active-class");
+    } else {
+      section.classList.remove("your-active-class");
+    }
+  };
+
+  // Scroll to anchor ID using scrollTO event
+
+  /**
+   * @description Scroll to section on menu click
+   * @param {Object} event - click
+   */
+  const scrollToSection = (event) => {
+    const anchor = event.target;
+
+    // Prevent clicking default behaviour
+    event.preventDefault();
+
+    // Get the section
+    let section = document.querySelector(anchor.hash);
+
+    // Scroll to section
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  };
+  /**
+   * End Main Functions
+   * Begin Events
+   *
+   */
+
+  // Build menu
+  const tempItemsList = createMenuItems(sectionsList);
+  navigationMenuList.appendChild(tempItemsList);
+
+  // Dinamically update menu
+  mainContent.addEventListener("DOMNodeInserted", updateMenuList);
+  mainContent.addEventListener("DOMNodeRemoved", updateMenuList);
+
+  // Set sections as active
 
   for (const section of sectionsList) {
-    const menuItem = createMenuItem(section);
-
-    tempList.append(menuItem);
+    document.addEventListener("scroll", (e) => {
+      toggleSectionActive(section);
+    });
   }
 
-  return tempList;
-};
+  // Scroll to section on link click
 
-/**
- * @description Update menu on section add/remove
- * @param {Object} event - DOMNodeRemoved or DOMNodeInserted
- */
-const updateMenuList = (event) => {
-  const section = event.target;
+  // Make a list of anchor links
+  let anchorsList = document.querySelectorAll('a[href^="#"]');
 
-  // check that the target node is a section
-  if (section.id.match(/^section/i)) {
-    if (event.type === "DOMNodeRemoved") {
-      // remove existing menu item corresponding to deleted section
-      removeMenuItem(section);
-    } else if (event.type === "DOMNodeInserted") {
-      // add menu item for added section
-      addMenuItem(section);
-    }
+  for (const anchor of anchorsList) {
+    anchor.addEventListener("click", scrollToSection);
   }
+
+  // Test performance end
+  const endTime = performance.now();
+  console.log("Time to run: " + (endTime - startTime) + " milliseconds.");
 };
 
-// Add class 'active' to section when near top of viewport
-
-/**
- * @description Add class 'active' to the section in viewport
- * @param {Object} event - scroll
- */
-const toggleSectionActive = (section) => {
-  if (isSectionInViewport(section)) {
-    section.classList.add("your-active-class");
-  } else {
-    section.classList.remove("your-active-class");
-  }
-};
-
-// Scroll to anchor ID using scrollTO event
-
-/**
- * @description Scroll to section on menu click
- * @param {Object} event - click
- */
-const scrollToSection = (event) => {
-  const anchor = event.target;
-
-  // Prevent clicking default behaviour
-  event.preventDefault();
-
-  // Get the section
-  let section = document.querySelector(anchor.hash);
-
-  // Scroll to section
-  section.scrollIntoView({
-    behavior: "smooth",
-    block: "center",
-  });
-};
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-const tempItemsList = createMenuItems(sectionsList);
-navigationMenuList.appendChild(tempItemsList);
-
-// Dinamically update menu
-mainContent.addEventListener("DOMNodeInserted", updateMenuList);
-mainContent.addEventListener("DOMNodeRemoved", updateMenuList);
-
-// Set sections as active
-
-for (const section of sectionsList) {
-  document.addEventListener("scroll", (e) => {
-    toggleSectionActive(section);
-  });
-}
-
-// Scroll to section on link click
-
-// Make a list of anchor links
-let anchorsList = document.querySelectorAll('a[href^="#"]');
-
-for (const anchor of anchorsList) {
-  anchor.addEventListener("click", scrollToSection);
-}
+// Run main function on DOMContentLoaded
+// Running the function in the head increases the performance
+document.addEventListener("DOMContentLoaded", function () {
+  main();
+});
